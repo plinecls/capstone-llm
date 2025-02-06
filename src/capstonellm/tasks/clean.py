080e3bfb8ecd90773c5453d7d6d65d7432d9de80
plinecls <72404169+plinecls@users.noreply.github.com>
2025-02-06 17:06:17 +0000

    Added read, clean, and main functions

--- src/capstonellm/tasks/clean.py
+++ src/capstonellm/tasks/clean.py
@@ -1,22 +1,74 @@
 import argparse
 import logging
-from pyspark.sql import SparkSession
+from typing import List
+from pyspark.sql import SparkSession, DataFrame
+import pyspark.sql.functions as psf
 from capstonellm.common.catalog import llm_bucket
 from capstonellm.common.spark import ClosableSparkSession
 
 logger = logging.getLogger(__name__)
 
-def clean(spark: SparkSession, environment: str, tag: str):
-    pass
+s3_bucket = "s3a://dataminded-academy-capstone-llm-data-us/"
 
-def main():
+def read(spark: SparkSession, json_path):
+    return spark.read.json(json_path)
+
+def clean(
+    df:DataFrame, col_to_explode:str, 
+    fields_to_extract:List, 
+    alias:str="items") -> DataFrame:
+
+    return (
+        df.select(
+            psf.explode(col_to_explode).alias(alias)
+        ).select(fields_to_extract)
+    )
+
+def main(spark: SparkSession, environment: str, tag: str, user: str):
+
+    questions = read(spark, json_path=f"{s3_bucket}/input/{tag}/questions.json")
+    answers = read(spark, json_path=f"{s3_bucket}/input/{tag}/answers.json")
+
+    cleaned_questions = (clean(
+        df=questions, col_to_explode="items", 
+        fields_to_extract=['items.question_id', 'items.title', 'items.body'], 
+        alias="items")
+        # .show()
+        )
+
+    cleaned_answers = (clean(
+        df=answers, col_to_explode="items", 
+        fields_to_extract=['items.answer_id', 'items.question_id', 'items.body'], 
+        alias="items")
+        .withColumnRenamed("body", "answer_body")
+        )
+
+    q_a = (
+        cleaned_questions.join(
+            cleaned_answers, 
+            on="question_id", 
+            how='inner'
+        )
+    )
+    
+    (q_a.repartition(q_a.count())
+        .write
+        .json(f"{s3_bucket}/cleaned/{user}/{tag}/"))
+
+
+    
+def execute():
     parser = argparse.ArgumentParser(description="capstone_llm")
     parser.add_argument(
         "-e", "--env", dest="env", help="environment we are executing in", required=False, default="local"
     )
     parser.add_argument(
         "-t", "--tag", dest="tag", help="the tag to process",
-        default="python-polars", required=False
+        default="pyspark", required=False
+    )
+    parser.add_argument(
+        "-u", "--user", dest="user", help="the user to write",
+        default="paulinec", required=False
     )
     logger.info("starting the cleaning job")
 
@@ -32,11 +84,11 @@ def main():
             .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4")
             .getOrCreate()
         )
-        clean(session, args.env, args.tag)
+        main(session, args.env, args.tag, args.user)
     else:
         with ClosableSparkSession("capstone_llm", spark_config=common_spark_config) as session:
-            clean(session, args.env, args.tag)
+            main(session, args.env, args.tag, args.user)
 
 
 if __name__ == "__main__":
-    main()
+    execute()
